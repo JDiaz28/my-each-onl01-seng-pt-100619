cab6d0e047d632809a124acd8ed904748ddf3fdc
Abraham Moreno <adiaz2821@gmail.com>
2019-11-12 13:36:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,24 @@
-def my_each # put argument(s) here
-  # code here
-end
+# put argument(s) here
+
+def list(people)
+  if block_given?
+  i = 0 
+
+  while i < people.length 
+    yield (people[i])
+    i = i + 1 
+  end 
+
+people 
+  else 
+  return "This"
+  end   
+end 
+
+ # code here
+ 
+["Bobby", "Esther", "Bryan"]
+list(["Bobby", "Esther", "Bryan"]) do |word|
+    return "Hi, #{word}"
+  
+end 
